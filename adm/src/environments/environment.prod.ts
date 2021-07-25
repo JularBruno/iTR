2763ffb0e7d49f8e9bd48e0713aa3b3263062517
jularbruno <brunojular00@gmail.com>
2021-07-25 19:58:51 -0300

    changed ports

--- adm/src/environments/environment.prod.ts
+++ adm/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // serverUrl: '/api',
-  // filesUrl: '/files'
-  serverUrl: 'http://192.168.0.30:8080/api',
-  filesUrl: 'http://192.168.0.30:8080/files',
+  serverUrl: '/api',
+  filesUrl: '/files'
+  // serverUrl: 'http://192.168.0.30:8080/api',
+  // filesUrl: 'http://192.168.0.30:8080/files',
 };
